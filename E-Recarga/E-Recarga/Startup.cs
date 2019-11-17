f40daa1736f0760fa1ac4ed4f90920315c20afd4
Skully <Wallace9655@gmail.com>
2019-11-17 11:02:32 +0000

    Added Git Ignore, Base Project and bootstrap themes

---
+++ E-Recarga/E-Recarga/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_Recarga.Startup))]
+namespace E_Recarga
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
