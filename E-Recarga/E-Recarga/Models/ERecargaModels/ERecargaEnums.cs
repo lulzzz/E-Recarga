3c9d66cd1f9ff1ee78909622c530a3317fd95c55
Skully <Wallace9655@gmail.com>
2019-12-12 00:07:07 +0000

    Added user to RoleEnum and fixed line ending with on ERecargaEnums

--- E-Recarga/E-Recarga/Models/ERecargaModels/ERecargaEnums.cs
+++ E-Recarga/E-Recarga/Models/ERecargaModels/ERecargaEnums.cs
@@ -19,6 +19,7 @@
     {
         Administrator,      //Manager (Adds and removes companies)
         CompanyManager,     //Can assign employees to a station, add, remove and edit stations (Can do everything a Employee can do).
-        Employee            //Can see, Edit and cancel appointments. Appointments cancelled by the Employee do not charge the user. Can also flag pods as inactive if they are broken
+        Employee,           //Can see, Edit and cancel appointments. Appointments cancelled by the Employee do not charge the user. Can also flag pods as inactive if they are broken
+        User
     }
 }
