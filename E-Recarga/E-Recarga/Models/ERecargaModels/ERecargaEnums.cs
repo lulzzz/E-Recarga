862bd0ba4e264eebe2eeeb16e71a485c7138c711
Joana <jbmaria.joana@gmail.com>
2019-12-17 03:05:03 +0000

    create && edit && details working

--- E-Recarga/E-Recarga/Models/ERecargaModels/ERecargaEnums.cs
+++ E-Recarga/E-Recarga/Models/ERecargaModels/ERecargaEnums.cs
@@ -1,4 +1,7 @@
-﻿namespace E_Recarga.Models
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace E_Recarga.Models
 {
     public enum PodTypeEnum
     {
@@ -21,5 +24,37 @@
         CompanyManager,     //Can assign employees to a station, add, remove and edit stations (Can do everything a Employee can do).
         Employee,           //Can see, Edit and cancel appointments. Appointments cancelled by the Employee do not charge the user. Can also flag pods as inactive if they are broken
         User
+    }
+
+    //Tradutor para HTML dos enums
+    public static class Enum_Dictionnary
+    {
+        public static IDictionary<string, string> Translator = new Dictionary<string, string>
+        {
+            { nameof(PodTypeEnum.Normal), "Normal" },
+            { nameof(PodTypeEnum.Fast), "Rápido" },
+
+            { nameof(AppointmentStatusEnum.Pending), "Pendente" },
+            { nameof(AppointmentStatusEnum.Ongoing), "A decorrer" },
+            { nameof(AppointmentStatusEnum.Completed), "Concluído" },
+            { nameof(AppointmentStatusEnum.Absent), "Ausente" },
+            { nameof(AppointmentStatusEnum.Cancelled), "Cancelado" },
+
+            { nameof(RoleEnum.Administrator), "Administrador" },
+            { nameof(RoleEnum.CompanyManager), "Gestor" },
+            { nameof(RoleEnum.Employee), "Funcionário" },
+            { nameof(RoleEnum.User).ToString(), "Utilizador" }
+        };
+
+        public static string GetKeyFromValue(string value)
+        {
+            int size = Translator.Count;
+            for (int i = 0; i < size; i++)
+            {
+                if (Translator[Translator.Keys.ElementAt(i)] == value)
+                    return Translator.Keys.ElementAt(i);
+            }
+            return "";
+        }
     }
 }
---
+++ E-Recarga/E-Recarga/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,69 @@
+﻿using E_Recarga.Models;
+using E_Recarga.Models.ERecargaModels;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Web.Mvc;
+
+namespace E_Recarga.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public EmployeeViewModel() {}
+        public EmployeeViewModel(Employee employee)
+        {
+            Name = employee.Name;
+            Email = employee.Email;
+            PhoneNumber = employee.PhoneNumber;
+            Employee = employee;
+        }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Nome")]
+        [StringLength(maximumLength: 100, ErrorMessage = "O {0} deve conter {2} a {1} caracteres", MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage = "O email deve seguir o formato: meuEmail@mail.pt")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Número de contacto")]
+        [RegularExpression("[0-9]{9}", ErrorMessage = "O número deve conter 9 dígitos.")]
+        public string PhoneNumber { get; set; }
+
+        //[Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Função")]
+        public string EmployeeType { get; set; }
+        
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [Display(Name = "Empresa")]
+        public int CompanyId { get; set; }
+
+        [Display(Name = "Estação")]
+        public int? StationId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public virtual IList<string> RoleEnums { get; set; } //string list
+
+        [ScaffoldColumn(false)]
+        public SelectList CompaniesList { get; set; }
+
+        [ScaffoldColumn(false)]
+        public SelectList StationsList { get; set; }
+
+        [ScaffoldColumn(false)]
+        public Employee Employee { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string Id { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string Username { get; set; }
+
+        [ScaffoldColumn(false)]
+        public string CompanyName { get; set; }
+    }
+}
