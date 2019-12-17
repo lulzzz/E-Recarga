0cf04c471b57aa5310432eddb4b6ec0feb2e18bf
Skully <Wallace9655@gmail.com>
2019-12-17 02:35:08 +0000

    fixed migrations, fixed stations and new config file fixed

--- E-Recarga/E-Recarga/Models/ERecargaModels/ERecargaDbContext.cs
+++ E-Recarga/E-Recarga/Models/ERecargaModels/ERecargaDbContext.cs
@@ -25,7 +25,6 @@ namespace E_Recarga.Models.ERecargaModels
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<PodType>()
                 .Property(s => s.Id)
@@ -35,56 +34,7 @@ namespace E_Recarga.Models.ERecargaModels
                 .Property(s => s.Id)
                 .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
 
-
-            modelBuilder.Entity<Appointment>()
-                 .HasRequired(x => x.Company)
-                 .WithMany(x => x.Appointments)
-                 .HasForeignKey(x => x.CompanyId)
-                 .WillCascadeOnDelete(true);
-
-            modelBuilder.Entity<Appointment>()
-                 .HasOptional(x => x.Station)
-                 .WithMany(x => x.Appointments)
-                 .HasForeignKey(x => x.StationId);
-
-            modelBuilder.Entity<Appointment>()
-                 .HasOptional(x => x.Pod)
-                 .WithMany(x => x.Appointments)
-                 .HasForeignKey(x => x.PodId);
-
-            modelBuilder.Entity<Price>()
-                 .HasRequired(x => x.Station)
-                 .WithMany(x => x.Prices)
-                 .HasForeignKey(x => x.StationId)
-                 .WillCascadeOnDelete(true);
-
-            modelBuilder.Entity<Station>()
-                 .HasMany(x => x.Pods)
-                 .WithRequired(x => x.Station)
-                 .HasForeignKey(x => x.StationId);
-
-            modelBuilder.Entity<Station>()
-                 .HasRequired(x => x.Company)
-                 .WithMany(x => x.Stations)
-                 .HasForeignKey(x => x.CompanyId)
-                 .WillCascadeOnDelete(true);
-
-            modelBuilder.Entity<Employee>()
-                 .HasRequired(x => x.Company)
-                 .WithMany(x => x.Employees)
-                 .HasForeignKey(x => x.CompanyId)
-                 .WillCascadeOnDelete(true);
-
-            modelBuilder.Entity<Employee>()
-                 .HasOptional(x => x.Station)
-                 .WithMany(x => x.Employees)
-                 .HasForeignKey(x => x.StationId);
-
-            modelBuilder.Entity<Appointment>()
-                .HasRequired(x => x.Status);
-
-            modelBuilder.Entity<Pod>()
-                .HasRequired(x => x.PodType);
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
