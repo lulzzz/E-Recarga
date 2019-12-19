70c8e94a95c7996e2817e9c46b6a7c124c46509c
Skully <Wallace9655@gmail.com>
2019-12-19 18:37:31 +0000

    Added custom validator for dates, Added Datepicker package, Example at /Appointments/Create

--- E-Recarga/E-Recarga/App_Start/BundleConfig.cs
+++ E-Recarga/E-Recarga/App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace E_Recarga
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/united.bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datetime").Include(
+                      "~/Scripts/moment*",
+                      "~/Scripts/bootstrap-datetimepicker*"));
         }
     }
 }
