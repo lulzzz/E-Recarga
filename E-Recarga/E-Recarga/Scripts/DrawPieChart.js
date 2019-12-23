e0d4ce593adb5b32a322287e8813099cad655833
Skully <Wallace9655@gmail.com>
2019-12-23 16:33:09 +0000

    Added JavaScript do draw profit chart

--- E-Recarga/E-Recarga/Scripts/DrawPieChart.js
+++ E-Recarga/E-Recarga/Scripts/DrawPieChart.js
@@ -1,4 +1,4 @@
-﻿function drawpiechart (id, data) {
+﻿function DrawOverviewPieChart (id, data) {
     var chart = new CanvasJS.Chart("overviewchart__" + id, {
         animationEnabled: true,
             title: {
@@ -7,7 +7,26 @@
         data: [{
             type: "pie",
             startAngle: 240,
-            yValueFormatString: "##0.00\"%\"",
+            yValueFormatString: "##0.00",
+            indexLabel: "{label} {y}",
+            dataPoints: data,
+        }]
+    });
+
+    chart.render();
+}
+
+
+function DrawProfitPieChart(id, data) {
+    var chart = new CanvasJS.Chart("monthlyProfitChart__" + id, {
+        animationEnabled: true,
+        title: {
+            text: "Lucro/Dia"
+        },
+        data: [{
+            type: "pie",
+            startAngle: 240,
+            yValueFormatString: "##0.00",
             indexLabel: "{label} {y}",
             dataPoints: data,
         }]
